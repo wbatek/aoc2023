15ba4289b4452a33accdcbbb4139c26fc2378203
wbatek <wojciech.batko02@gmail.com>
2023-12-01 00:44:44 +0100

    add day01 structure

---
+++ day01/main.py
@@ -0,0 +1,15 @@
+def part_one(filename: str):
+    pass
+
+def part_two(filename: str):
+    pass
+
+
+if __name__ == "__main__":
+    input_path = "./day_01/input.txt"
+    f = open("./day_01/output1.txt", "w")
+    # f.write(part_one(input_path))
+
+    f = open("./day_01/output2.txt", "w")
+    # f.write(part_two(input_path))
+    
